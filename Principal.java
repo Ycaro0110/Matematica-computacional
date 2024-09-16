150100731f5b837030c747b7999536850af573c1
Ycaro Entriel <86209679+Ycaro0110@users.noreply.github.com>
2024-09-16 16:17:31 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,42 +1,10 @@
 import org.mariuszgromada.math.mxparser.*;
+
 import java.util.Scanner;
 
-public class Main {
-
-    public static double bisseccao(double x1, double x2, double e, String func) {
-        Function f = new Function("f(x) = " + func);
-        f.checkSyntax();
-        mXparser.consolePrintln(f.getErrorMessage());
-
-        double max_iteracao = Math.round(Math.log(x2 - x1) - Math.log(e)) / Math.log(2);
-        double x_medio = 0;
-        double num_iteracao = 0;
-
-        while ((x2 - x1) / 2 >= e || (num_iteracao < max_iteracao)) {
-
-            double bolzano = f.calculate(x1) * f.calculate(x2);
-            if (bolzano > 0) {
-                return 0;
-            }
-            x_medio = (x1 + x2) / 2;
-
-            double teste = f.calculate(x1) * f.calculate(x_medio);
-
-            if (f.calculate(x_medio) == 0) {
-                return x_medio;
-            }
-            if (teste < 0) {
-                x2 = x_medio;
-            } else {
-                x1 = x_medio;
-            }
-            num_iteracao++;
-        }
-        return x_medio;
-    }
+public class Principal {
 
     public static void main(String[] args) {
-
 
         boolean isCallSuccessful = License.iConfirmNonCommercialUse("Ycaro");
         boolean isConfirmed = License.checkIfUseTypeConfirmed();
@@ -45,6 +13,8 @@ public class Main {
         mXparser.consolePrintln("isCallSuccessful = " + isCallSuccessful);
         mXparser.consolePrintln("isConfirmed = " + isConfirmed);
         mXparser.consolePrintln("message = " + message);
+
+        Funcao metodo = new Funcao();
 
         Scanner teclado = new Scanner(System.in);
         System.out.println("expressão: ");
@@ -58,7 +28,7 @@ public class Main {
 
         double erro = teclado.nextDouble();
 
-        double result = bisseccao(x1, x2, erro, expressao);
+        double result = metodo.bisseccao(x1, x2, erro, expressao);
 
         if (result == 0) {
             System.out.println("Não há raízes neste intervalo");
