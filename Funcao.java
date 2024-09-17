d1fae1d3c923f72a7a4f07432dd5ed6b9d8decda
Ycaro Entriel <86209679+Ycaro0110@users.noreply.github.com>
2024-09-17 11:14:34 -0300

    Update Funcao.java

--- Funcao.java
+++ Funcao.java
@@ -10,29 +10,48 @@ public class Funcao {
 
         //calcula a estimativa de iterações
         double max_iteracao = Math.round(Math.log(x2 - x1) - Math.log(e)) / Math.log(2);
+
+        //calcula a metade do intervalo (a+b)/2
         double x_medio = 0;
+        double x_old;
+        double erro_estimado = 0;
         double num_iteracao = 0;
 
-        while ((x2 - x1) / 2 >= e || (num_iteracao < max_iteracao)) {
+        //continua as iterações enquanto o erro estimado for maior que o erro desejado
+        //ou enquanto o numero de iterações for menor que o esperado
+        while (erro_estimado >= e || (num_iteracao <= max_iteracao)) {
 
+            //Verifica se há uma raiz localizada no intervalo, através do teorema de bolzano
             double bolzano = f.calculate(x1) * f.calculate(x2);
             if (bolzano > 0) {
                 return 0;
             }
+
+            //armazena o x medio da iteração anterior em uma variavel
+            x_old = x_medio;
+
+            //calcula um novo x medio
             x_medio = (x1 + x2) / 2;
 
-            double teste = f.calculate(x1) * f.calculate(x_medio);
-
+            //caso o x seja a raiz, ele é retornado
             if (f.calculate(x_medio) == 0) {
                 return x_medio;
             }
+
+            //faz a verificação dos sinais e aponta qual será o novo intervalo superior ou inferior
+            double teste = f.calculate(x1) * f.calculate(x_medio);
             if (teste < 0) {
                 x2 = x_medio;
             } else {
                 x1 = x_medio;
             }
+
+            //faz o erro estimado entre o x_medio atual e o anterior
+            erro_estimado = Math.abs(x_medio - x_old) / Math.abs(x_medio);
             num_iteracao++;
         }
+
+        //ao final das iterações retorna a raiz encontrada
         return x_medio;
     }
 
