3581d1230fa60a3a0de23c72d99b2394aac6acc0
Ycaro Entriel <86209679+Ycaro0110@users.noreply.github.com>
2024-09-21 14:47:00 -0300

    Update Funcao.java

--- Funcao.java
+++ Funcao.java
@@ -3,56 +3,59 @@ import org.mariuszgromada.math.mxparser.mXparser;
 
 public class Funcao {
 
-    public double bissecao(double x1, double x2, double e, String func) {
+    public double bisseccao(double x1, double x2, double e, String func) {
         Function f = new Function("f(x) = " + func);
+
         f.checkSyntax();
+
         mXparser.consolePrintln(f.getErrorMessage());
 
         //calcula a estimativa de iterações
-        double max_iteracao = Math.round(Math.log(x2 - x1) - Math.log(e)) / Math.log(2);
+        int max_iteracao = (int) (Math.ceil(Math.log(x2 - x1) - Math.log(e)) / Math.log(2));
 
         //calcula a metade do intervalo (a+b)/2
         double x_medio = 0;
         double x_old;
-        double erro_estimado = 0;
-        double num_iteracao = 0;
+        double erro_estimado;
+        int num_iteracao = 0;
 
         //continua as iterações enquanto o erro estimado for maior que o erro desejado
-        //ou enquanto o numero de iterações for menor que o esperado
-        while (erro_estimado >= e || (num_iteracao <= max_iteracao)) {
+        //ou enquanto enquanto o numero de iterações for menor que o esperado
 
+
+        do {
             //Verifica se há uma raiz localizada no intervalo, através do teorema de bolzano
             double bolzano = f.calculate(x1) * f.calculate(x2);
             if (bolzano > 0) {
                 return 0;
             }
 
-            //armazena o x medio da iteração anterior em uma variavel
+            //armazena o x_medio da iteração anterior em uma variavel
             x_old = x_medio;
 
-            //calcula um novo x medio
+            //calcula um novo x_medio
             x_medio = (x1 + x2) / 2;
+            num_iteracao++;
 
-            //caso o x_medio seja a raiz, ele é retornado
+            //caso o x seja a raiz, ele é retornado
             if (f.calculate(x_medio) == 0) {
                 return x_medio;
             }
 
             //faz a verificação dos sinais e aponta qual será o novo intervalo superior ou inferior
             double teste = f.calculate(x1) * f.calculate(x_medio);
-            if (teste < 0) {
-                x2 = x_medio;
-            } else {
+            if (teste > 0) {
                 x1 = x_medio;
+            } else {
+                x2 = x_medio;
             }
 
             //faz o erro estimado entre o x_medio atual e o anterior
-            erro_estimado = Math.abs(x_medio - x_old) / Math.abs(x_medio);
-            num_iteracao++;
-        }
 
+            erro_estimado = x_medio - x_old; // erro absoluto
+
+        } while (erro_estimado > e || num_iteracao < max_iteracao);
         //ao final das iterações retorna a raiz encontrada
         return x_medio;
     }
-
 }
